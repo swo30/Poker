1dc21eb5faac099b151bd58a2ccae36a90adf008
Stixon <20617870+stixon@users.noreply.github.com>
2020-03-17 16:07:29 -0400

    Modified royal flush calculation

--- app/src/main/java/com/example/poker/math.java
+++ app/src/main/java/com/example/poker/math.java
@@ -10,20 +10,18 @@ public class math {
    }
    
    static double comb(int n , int r){
+	   if(r ==0) return 1;
+	   if(r<0 || n <=0) return 0;
 	   return factorial(n) / (factorial(r) * factorial(n-r));
    }
    
    static double perm(int n, int r){
+	   if(r ==0) return 1;
+	   if(r<0 || n <=0) return 0;
 		return factorial(n) / factorial(n-r);
    }
    
    public static void main(String args[]) {
-      /* int n = 7, r = 3, comb, per;
-      per = factorial(n) / factorial(n-r);
-      System.out.println("Permutation: " + per);
-      comb = factorial(n) / (factorial(r) * factorial(n-r));
-      System.out.println("Combination: " + comb); */
-	  
 	  System.out.println("Royal Flush: ");
 	  RoyalFlush();
 	  System.out.println("6 cards ");
@@ -31,12 +29,34 @@ public class math {
    }
    
    public static void RoyalFlush(){
-	   double comb;
-	   comb = comb(52,5);
-	   System.out.println((4 / comb(52,5)));
-	   System.out.println((4*comb(52,2) / comb(52,7)));
-	   System.out.println((4*comb(52,1) / comb(52,7)));
-	   System.out.println((4*comb(52,1) / comb(52,6)));
 	   
+	   // Parameters:
+	   int cardsDeployed =2;
+	   int[] cardsSuit = {0,1,0,0};
+	   
+	   // Code:
+	   int sumOfChances =0;
+	   if (7-cardsDeployed+cardsSuit[0] >=4){
+		   sumOfChances += cardsSuit[0]*comb(52-cardsDeployed-4,7-4-cardsDeployed);
+	   }
+	   if (7-cardsDeployed+cardsSuit[1] >=3){
+		   sumOfChances += cardsSuit[1]*comb(52-cardsDeployed-3,7-3-cardsDeployed);
+	   }
+	   if (7-cardsDeployed+cardsSuit[2] >=2){
+		   sumOfChances += cardsSuit[2]*comb(52-cardsDeployed-2,7-2-cardsDeployed);
+	   }
+	   if (7-cardsDeployed+cardsSuit[3] >=1){
+		   sumOfChances += cardsSuit[3]*comb(52-cardsDeployed-1,7-1-cardsDeployed);
+	   }
+	   if (7-cardsDeployed >=5){
+		   int count =0;
+		   for (int i =0; i<4;i++){
+			   count += cardsSuit[i];
+		   }
+		   sumOfChances += (4-count)*comb(52-5-cardsDeployed,7-5-cardsDeployed);
+	   }
+	   
+	   System.out.println("Total Chances " + sumOfChances + " on " + comb(52-cardsDeployed,7-cardsDeployed) + " = " + (sumOfChances/comb(52-cardsDeployed,7-cardsDeployed)));   
+
    }
 }
