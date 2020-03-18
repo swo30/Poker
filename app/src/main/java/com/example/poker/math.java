9938dcf17de3c73192d655e1d844b7687fa91d3e
Stixon <20617870+stixon@users.noreply.github.com>
2020-03-18 14:50:06 -0400

    Finished Straight Flush

--- app/src/main/java/com/example/poker/math.java
+++ app/src/main/java/com/example/poker/math.java
@@ -22,24 +22,26 @@ public class math {
    }
    
    public static void main(String args[]) {
+	  String[] myCards = {"11","12", "13","14"};
 	  System.out.println("Royal Flush: ");
-	  RoyalFlush();
-	  System.out.println("6 cards ");
-	  System.out.println("5 cards ");
+	  RoyalFlush(myCards);
+	  System.out.println("Straight Flush: ");
+	  StraightFlush(myCards);
+	  //System.out.println("6 cards ");
+	  //System.out.println("5 cards ");
    }
    
-   public static void RoyalFlush(){
-	   String[] myCards = {"11","22", "A1", "B1", "C1", "D1"};
+   public static void RoyalFlush(String[] myCards){
 	   
 	   int cardsDeployed = myCards.length;
 	   int count;
-	   int[] cardsSuit = {0,0,0,0};
+	   int[] cardsSuit = {0,0,0,0,0};
 	   
 	   // Parse card hand
-		for (int type =48; type<52;type++){
+		for (int type =49; type<53;type++){
 			count =-1;
 			for (String cards : myCards){
-				if (cards.charAt(1) == type && (cards.charAt(0) == 'A' || cards.charAt(0) == 'B' || cards.charAt(0) == 'C' || cards.charAt(0) == 'D' || cards.charAt(0) == 'E'))
+				if (cards.charAt(1) == type && (cards.charAt(0) == 'A' || cards.charAt(0) == 'B' || cards.charAt(0) == 'C' || cards.charAt(0) == 'D' || cards.charAt(0) == '1'))
 					count+=1;
 			}
 			if (count != -1)
@@ -71,5 +73,40 @@ public class math {
 	   
 	   System.out.println("Total Chances " + sumOfChances + " on " + comb(52-cardsDeployed,7-cardsDeployed) + " = " + (sumOfChances/comb(52-cardsDeployed,7-cardsDeployed)));   
 
+   }
+   
+   public static void StraightFlush(String[] myCards){
+	   
+	   
+	   int[] cardsSuit = {0,0,0,0,0}; //0,1,2,3
+	   int cardsDeployed = myCards.length;
+	   int count=0;
+	   for (int type = 49; type <53; type++){
+		   for (int i = 1; i<10; i++){
+			   count =0;
+			   for (int e = i; e<i+5; e++){
+				   for (String cards : myCards){
+					   if (cards.charAt(1) ==type && cards.charAt(0) == 48+e){
+						   count +=1;
+					   }
+				   }
+			   }
+			   cardsSuit[count]+=1;
+		   }
+	   }
+	   
+	   System.out.println(cardsSuit[0]  + " " + cardsSuit[1]  + " " + cardsSuit[2]  + " " + cardsSuit[3]);
+	   
+	   //Statistics
+	   int sumOfChances =0;
+	   sumOfChances += cardsSuit[0]*comb(52-5-cardsDeployed,7-5-cardsDeployed);
+	   sumOfChances += cardsSuit[1]*comb(52-4-cardsDeployed,7-4-cardsDeployed);
+	   sumOfChances += cardsSuit[2]*comb(52-3-cardsDeployed,7-3-cardsDeployed);
+	   sumOfChances += cardsSuit[3]*comb(52-2-cardsDeployed,7-2-cardsDeployed);
+	   sumOfChances += cardsSuit[4]*comb(52-1-cardsDeployed,7-1-cardsDeployed);
+	   
+	   System.out.println("Total Chances " + sumOfChances + " on " + comb(52-cardsDeployed,7-cardsDeployed) + " = " + (sumOfChances/comb(52-cardsDeployed,7-cardsDeployed)));   
+
+	   
    }
 }
