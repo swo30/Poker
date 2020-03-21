32967f27a4b782f8ba7d94c6fc99171377a165ee
Simon Duquette <simon.duquette@hotmail.com>
2020-03-20 20:15:32 -0700

    added all textviews and put them ready to be displayed

--- app/src/main/java/com/example/poker/tablecards.java
+++ app/src/main/java/com/example/poker/tablecards.java
@@ -14,28 +14,45 @@ public class tablecards extends AppCompatActivity {
 
     Intent intent;
     String[] myCards;
-    TextView RFChanceText;
+
+
+    TextView pokerHands[] = new TextView[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tablecards);
         intent = getIntent();
-        RFChanceText = findViewById(R.id.RFChance);
 
+        pokerHands[0] = findViewById(R.id.RFChance);
+        pokerHands[1] = findViewById(R.id.SFChance);
+        pokerHands[2] = findViewById(R.id.FKChance);
+        pokerHands[3] = findViewById(R.id.FHChance);
+        pokerHands[4] = findViewById(R.id.FlushChance);
+        pokerHands[5] = findViewById(R.id.StraightChance);
+        pokerHands[6] = findViewById(R.id.TKChance);
+        pokerHands[7] = findViewById(R.id.TPChance);
+        pokerHands[8] = findViewById(R.id.OPChance);
+        pokerHands[9] = findViewById(R.id.HCText);
 
         String myCardsStr = intent.getStringExtra("myCardsStr");
         myCards = splitToNChar(myCardsStr, 2);
 
         updateText();
-
     }
 
     private void updateText(){
         double chances[];
-        chances = math.RoyalFlush(myCards);
-        RFChanceText.setText(String.valueOf(chances[2]));
 
+        //for (int i=0;i<10;i++){}
+        //chances = math.RoyalFlush(myCards);
+        //pokerHands[0].setText(String.valueOf(100*chances[2])+"%");
+
+        chances = math.StraightFlush(myCards);
+        pokerHands[1].setText(String.valueOf(100*chances[2])+"%");
+
+        chances = math.FourOfKind(myCards);
+        pokerHands[2].setText((100*chances[2])+"%");
 
     }
 
