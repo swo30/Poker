53040abb1e997918479a535b1ab4322039318511
Simon Duquette <simon.duquette@hotmail.com>
2020-04-27 19:41:15 -0400

    added multiple functions

--- simulation.m
+++ simulation.m
@@ -2,22 +2,46 @@ parpool(12);
 tic
 sims = 10000;
 %numCards = input("How many cards: ");
-numCards = 5;
-counterVec = zeros(1,sims/10);
-
-parfor i = 1:(sims)
-    counter = 0;
-    for j = 1:(sims)
-        myCards = randperm(52, numCards);
-        counter = FullHouse(myCards,counter);
-    end
-    counterVec(i) = counter;
+numCards = 7;
+cnt1 = 0;
+cnt2 = 0;
+cnt3 = 0;
+cnt4 = 0;
+cnt5 = 0;
+counter =0;
+parfor i = 1:(sims*sims)
+    myCards = randperm(52, numCards);
+	cnt5 = cnt5 + FourKind(myCards,counter);
+    cnt4 = cnt4 + FullHouse(myCards,counter);
+    cnt3 = cnt3 + ThreeKind(myCards,counter);
+    cnt2 = cnt2 + TwoPair(myCards,counter);
+    cnt1 = cnt1 + Pair(myCards,counter);
 end
 
-percentage = sum(counterVec,'all')/(sims*sims);
-fprintf("%.4f%%\n",100*percentage)
+counterVec = [cnt1,cnt2,cnt3,cnt4,cnt5];
+percentages = counterVec./(sims*sims);
+fprintf("%.4f%%\n",100.*percentages)
 delete(gcp('nocreate'))
 toc
+
+function counter = FourKind(myCards,counter)
+    suits = ceil(myCards./13);
+    values = myCards - 13.*(suits-1);
+
+    cardsValue = [0,0,0,0,0]; %%Non Values,singles,pairs,3kind,4kind
+    map = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+    for i = 1:length(myCards)
+        map(values(i)) = map(values(i)) + 1;
+    end
+
+    for i = 1:13
+        cardsValue(map(i)+1) = cardsValue(map(i)+1) + 1;
+    end
+    if (cardsValue(5)>0)
+        counter = counter + 1;
+    end
+end
 
 function counter = FullHouse(myCards,counter)
 	suits = ceil(myCards./13);
@@ -34,8 +58,65 @@ function counter = FullHouse(myCards,counter)
         cardsValue(map(i)+1) = cardsValue(map(i)+1) + 1;
     end
     % 4k&3k or 3k&3k or 3k&pair or 4k&pair
-    if ( ((cardsValue(5)>0)&&(cardsValue(4)>0)) || ((cardsValue(4)>0)&&(cardsValue(4)>0)) || ((cardsValue(4)>0)&&(cardsValue(3)>0)) || ((cardsValue(5)>0)&&(cardsValue(3)>0)))
+    if (((cardsValue(5)>0)&&(cardsValue(4)>0)) || ((cardsValue(4)>0)&&(cardsValue(4)>0)) || ((cardsValue(4)>0)&&(cardsValue(3)>0)) || ((cardsValue(5)>0)&&(cardsValue(3)>0)))
+    %if (((cardsValue(4)>0)&&(cardsValue(4)>0)) || ((cardsValue(4)>0)&&(cardsValue(3)>0)))
         counter = counter + 1;
     end
 end
 
+function counter = ThreeKind(myCards,counter)
+    suits = ceil(myCards./13);
+    values = myCards - 13.*(suits-1);
+
+    cardsValue = [0,0,0,0,0]; %%Non Values,singles,pairs,3kind,4kind
+    map = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+    for i = 1:length(myCards)
+        map(values(i)) = map(values(i)) + 1;
+    end
+
+    for i = 1:13
+        cardsValue(map(i)+1) = cardsValue(map(i)+1) + 1;
+    end
+    if (cardsValue(4)>0)
+        counter = counter + 1;
+    end
+end
+
+function counter = TwoPair(myCards,counter)
+    suits = ceil(myCards./13);
+    values = myCards - 13.*(suits-1);
+
+    cardsValue = [0,0,0,0,0]; %%Non Values,singles,pairs,3kind,4kind
+    map = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+    for i = 1:length(myCards)
+        map(values(i)) = map(values(i)) + 1;
+    end
+
+    for i = 1:13
+        cardsValue(map(i)+1) = cardsValue(map(i)+1) + 1;
+    end
+    if (cardsValue(3)>1)
+        counter = counter + 1;
+    end
+end
+
+function counter = Pair(myCards,counter)
+    suits = ceil(myCards./13);
+    values = myCards - 13.*(suits-1);
+
+    cardsValue = [0,0,0,0,0]; %%Non Values,singles,pairs,3kind,4kind
+    map = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+    for i = 1:length(myCards)
+        map(values(i)) = map(values(i)) + 1;
+    end
+
+    for i = 1:13
+        cardsValue(map(i)+1) = cardsValue(map(i)+1) + 1;
+    end
+    if (cardsValue(3)>0)
+        counter = counter + 1;
+    end
+end
